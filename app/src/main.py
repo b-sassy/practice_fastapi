4b558d22cab211e40ac76af58d5ad9ac449fd335
Your Name <you@example.com>
2024-02-28 17:50:26 +0900

    fastapi課題2

--- app/src/main.py
+++ app/src/main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.post("/echo/")
+def echo(message: str):
+    return {"content": message}
+
+@app.get("/hoge/")
+def hoge():
+    with open("./hoge.txt") as f:
+        content = f.read()
+    return {"content": content}
